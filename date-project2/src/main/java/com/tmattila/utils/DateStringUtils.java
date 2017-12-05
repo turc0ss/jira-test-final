287958733148cb84c7e714d6aec8aab84f85f259
tapsa <tapsa872@gmail.com>
2017-12-05 07:38:33 +0200

    DJ-1 #comment layout change 555 #time 5m #done

--- date-project2/src/main/java/com/tmattila/utils/DateStringUtils.java
+++ date-project2/src/main/java/com/tmattila/utils/DateStringUtils.java
@@ -7,7 +7,7 @@ package com.tmattila.utils;
  */
 public enum DateStringUtils {
 
-	HEADER_TEXT("Welcome to date storage application ---- jira test 4 ----"),
+	HEADER_TEXT("Welcome to date storage application ---- jira test 5555 ----"),
 	BUTTON_TEXT("Click to save the time of button press to database"),
 	DATE_TEXT("Date and time of button press: "),
 	DATE_TITLE("DATE"),
